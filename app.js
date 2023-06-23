2b2b38b11203ed3aefb1bbe5e171c7ab5b4f107b
sindy425 <cindyhernandez425@gmail.com>
2023-06-23 17:04:08 -0600

    USERNAME-EXA

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ app.listen(port, function () {
 
 var Connection = require('tedious').Connection;
 var config = {
-    server: 'sddd\SQLEXPRESS01',
+    server: 'SINDY\SQLEXPRESS01',
     authentication: {
         type: 'default',
         options: {
-            userName: 'SINDY\hern',
+            userName: 'SINDY\herna',
             password: 'sindy2023'
         }
 
